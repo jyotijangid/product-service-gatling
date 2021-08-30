ddfcb43a781b0dfc02b6b058bbb3b5a2b32e6731
Jyoti Jangid <jyotik.cse17@nituk.ac.in>
2021-08-30 18:13:41 +0530

    redis-server

--- src/test/scala/simulations/ProductInventorySimulation.scala
+++ src/test/scala/simulations/ProductInventorySimulation.scala
@@ -6,7 +6,7 @@ import io.gatling.http.Predef._
 class ProductInventorySimulation extends Simulation {
 
   //conf
-  val value_conf = http.baseUrl("http://localhost:8081")
+  val value_conf = http.baseUrl("http://localhost:9293")
     .header("Accept",value="application/json")
     .header(name="content-type", value ="application/json")
 
@@ -14,24 +14,24 @@ class ProductInventorySimulation extends Simulation {
   //scenario
   val scn = scenario("Get all Product Inventory")
     .exec(http("get all Product Inventory details")
-      .get("/rvy/api/pis/v1/products")
+      .get("/pis/v1/products")
       .check(status is 200)
 
     )
 	 
     .exec(http("Get Product Inventory By Id")
-      .get("/rvy/api/pis/v1/product/5720")
+      .get("/pis/v1/product/168402")
       .check(status is 200)
     )
 	
 	.exec(http("Post Product Inventory")
-      .post("/rvy/api/pis/v1/product")
+      .post("/pis/v1/product")
       .body(RawFileBody(filePath = "./src/test/resources/bodies/addProductInventory.json")).asJson
       .header(name="content-type",value = "application/json")
       .check(status is 200))
 
 	.exec(http("Update Product Inventory")
-      .post("/rvy/api/pis/v1/product")
+      .post("/pis/v1/product")
       .body(RawFileBody(filePath = "./src/test/resources/bodies/updateProductInventory.json")).asJson
       .header(name="content-type",value = "application/json")
       .check(status is 200))
