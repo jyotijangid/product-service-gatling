1532a9c5a49b9f7e8002f289d6827ef75e567cfb
Jyoti Jangid <71806907+jyotijangid@users.noreply.github.com>
2021-09-03 19:41:47 +0530

    Update ProductSimulation.scala

--- src/test/scala/simulations/ProductSimulation.scala
+++ src/test/scala/simulations/ProductSimulation.scala
@@ -40,7 +40,7 @@ class ProductSimulation extends Simulation {
 
   //setup
 //   setUp(scn.inject(atOnceUsers(users=100))).protocols(value_conf)
-	setUp(scn.inject(constantConcurrentUsers(412) during(30))).protocols(value_conf)
+	setUp(scn.inject(constantConcurrentUsers(500) during(25))).protocols(value_conf)
 
 
 
